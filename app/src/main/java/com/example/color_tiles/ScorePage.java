5f07f94aabebf7d6ba35b707514f9ab2c4eee0d1
florian_G <florian.gouverneur@reseau.eseo.fr>
2024-04-15 08:55:54 +0200

    Page d'accueil (problème de couleur résolu)

---
+++ app/src/main/java/com/example/color_tiles/ScorePage.java
@@ -0,0 +1,41 @@
+package com.example.color_tiles;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.color_tiles.databinding.ActivityScorePageBinding;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ScorePage extends AppCompatActivity {
+    private ActivityScorePageBinding binding;
+    private List<Fragment_Nom_Score> fragments;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_page);
+        fragments = new ArrayList<>();
+        fragments.add(Fragment_Nom_Score.newInstance(1,"gouverneur",30));
+        fragments.add(Fragment_Nom_Score.newInstance(2,"president",12));
+        fragments.add(Fragment_Nom_Score.newInstance(3,"ministre",10));
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        for(Fragment_Nom_Score frag : fragments) {
+            ft.add(R.id.FragLayout,frag);
+        }
+        ft.commit();
+    }
+    /*
+    @Override
+    protected void onResume() {
+        super.onResume();
+        binding.button.setOnClickListener(v -> {
+                Intent intent = new Intent(ScorePage.this, MainActivity.class);
+                startActivity(intent);
+        });
+    }*/
+}
